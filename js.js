4ce43decbb7257350702718d45e1473ddbf35009
NishantThapa <nishantthapa@icloud.com>
2017-08-03 15:32:59 +0530

    comments added

--- js.js
+++ js.js
@@ -1,11 +1,15 @@
+//create a function
 function loadDoc() {
+  //create a variable and assign new to it
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+    //check if responce is ready 
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("demo").innerHTML =
       this.responseText;
     }
   };
+  //send request to get a responce from server
   xhttp.open("GET", "txt.txt", true);
   xhttp.send();
 }
